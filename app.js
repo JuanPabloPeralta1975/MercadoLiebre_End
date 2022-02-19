bc4da60384d66d542f398e54fdf56a4e5164e017
JuanPabloPeralta1975 <peraltaabogado@hotmail.com>
2022-02-19 15:10:00 -0300

    mas cambios Leo

--- app.js
+++ app.js
@@ -10,14 +10,32 @@ app.listen(process.env.PORT || 3000, function () {
     console.log('Mercado Liebre Corriendo');
 });
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
+app.get("/", (req, res) => {
+    let patheoGet = path.resolve(__dirname, "./views/home.html");
+    res.sendFile(patheoGet);
+})
 
-app.get('/login', (req,res)=>{
+app.get("/register.html", (req, res) => {
+    let patheoGet = path.resolve(__dirname, "./views/register.html");
+    res.sendFile(patheoGet);
+})
+
+app.get("/login.html", (req, res) => {
+    let patheoGet = path.resolve(__dirname, "./views/login.html");
+    res.sendFile(patheoGet);
+})
+
+
+
+
+/* app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+}); */
+
+/* app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
 
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
-});
+}); */
